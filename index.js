bfe7027e6fe3fd06129f09f48af6b442a7830880
Airtribunal <airtribunal@gmail.com>
2022-10-11 16:03:03 +0300

    game-beginning

--- index.js
+++ index.js
@@ -11,10 +11,62 @@ const bot = new TelegramApi(token, {
 
 const chats = {}
 
+// Play Again Options
+const againOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{
+                text: "Play again!",
+                callback_data: "/again"
+            }],
+        ]
+    })
+}
 
 // Game Options
 const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{
+                text: "1",
+                callback_data: "1"
+            }, {
+                text: "2",
+                callback_data: "2"
+            }, {
+                text: "3",
+                callback_data: "3"
+            }],
+            [{
+                text: "1",
+                callback_data: "1"
+            }, {
+                text: "4",
+                callback_data: "4"
+            }, {
+                text: "5",
+                callback_data: "5"
+            }],
+            [{
+                text: "7",
+                callback_data: "7"
+            }, {
+                text: "8",
+                callback_data: "8"
+            }, {
+                text: "9",
+                callback_data: "9"
+            }]
+        ]
+    })
+}
 
+// Start the game 
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, "Try to guess what number from 0 to 9 I've chosen!", gameOptions);
+    const randomNumber = Math.floor(Math.random() * 10);
+    chats[chatId] = randomNumber;
+    await bot.sendMessage(chatId, "Waiting for your guess")
 }
 
 // Start
@@ -38,28 +90,34 @@ function start() {
         const text = msg.text;
         const chatId = msg.from.id
         const firstName = msg.from.first_name
-        
+
         if (text === "/start") {
             await bot.sendMessage(chatId, `Hello ${firstName}! You've just started using the bot. Please, check other bot commands`)
             return bot.sendSticker(chatId, "https://tlgrm.eu/_/stickers/f7e/fba/f7efbacf-9817-4b7e-8e07-dac0cf0430d1/192/5.webp")
         }
-        
+
         if (text === "/info") {
             return bot.sendMessage(chatId, `Your name is ${firstName}`)
         }
 
         if (text === "/game") {
-            await bot.sendMessage(chatId, "Try to guess what number from 0 to 9 I've chosen!");
-            const randomNumber = Math.floor(Math.random() * 10);
-            chats[chatId] = randomNumber;
-            console.log(chats);
-            return bot.sendMessage(chatId, "Waiting for your guesses")
+            return startGame(chatId)
         }
 
         return bot.sendMessage(chatId, `I don't get it. Probably, It's not the command`)
 
     })
 
+    bot.on("callback_query", msg => {
+        const data = msg.data
+        const chatId = msg.message.chat.id
+
+        if (data == "/again") {
+            return startGame(chatId)
+        }
+
+        return bot.sendMessage(chatId, `User has clicked on the button ${data}`, againOptions)
+    })
 }
 
 start()
